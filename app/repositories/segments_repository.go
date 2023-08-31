6c7ca59a60a5f042d2e6a1d5f6e39dcfdf39a9ad
Tulenien <timofey.evsigneev@gmail.com>
2023-08-31 22:47:45 +0200

    feat: add removing segments method & test

--- app/repositories/segments_repository.go
+++ app/repositories/segments_repository.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"fmt"
+	"log"
 
     "gorm.io/gorm"
     "github.com/Tulenien/Avito-backend-trainee-assignment-2023/app/models"
@@ -17,32 +18,55 @@ func NewSegmentsRepository(db *gorm.DB) *SegmentsRepository {
 	}
 }
 
-func (ur *SegmentsRepository) Create(segment models.Segments) (models.Segments, error) {
-	err := ur.db.Create(&segment).Error
+func (sr *SegmentsRepository) Create(segment models.Segments) (models.Segments, error) {
+	err := sr.db.Create(&segment).Error
 	if err != nil {
 		return models.Segments{}, err
 	}
 	return segment, err
 }
 
-func (ur *SegmentsRepository) FindByID(id uint) (models.Segments, error) {
+func (sr *SegmentsRepository) FindByID(id uint) (models.Segments, error) {
 	entity := models.Segments{
 		ID: uint(id),
 	}
-	err := ur.db.First(&entity).Error
+	err := sr.db.First(&entity).Error
 	if err != nil {
 		return models.Segments{}, fmt.Errorf(`segment with id=%d not found: %w`, id, err)
 	}
 	return entity, err
 }
 
-func (ur *SegmentsRepository) FindByName(name string) (models.Segments, error) {
+func (sr *SegmentsRepository) FindByName(name string) (models.Segments, error) {
 	entity := models.Segments{
 		Name: name,
 	}
-	err := ur.db.First(&entity).Error
+	err := sr.db.First(&entity).Error
 	if err != nil {
 		return models.Segments{}, fmt.Errorf(`segment with name=%s not found: %w`, name, err)
 	}
 	return entity, err
+}
+
+func (sr *SegmentsRepository) RemoveSegment(name string) error {
+	segment, err := sr.FindByName(name)
+	if err != nil {
+		return fmt.Errorf(`segment with name=%s not found: %w`, name, err)
+	}
+
+	var users []models.Users
+	if err = sr.db.Joins("JOIN users_segments on users_segments.user_id=users.id").
+			 Joins("JOIN segments on segments.id=users_segments.segment_id").
+			 Where("segments.name=?", name).
+			 Find(&users).Error; err != nil {
+				log.Fatal(err)
+				return fmt.Errorf(`error on finding segment with name=%s: %w`, name, err)
+	 		 }
+	for _, value := range users {
+		fmt.Println("%w\n", value)
+		models.RemoveSegment(&value, segment)
+		sr.db.Save(&value)
+	}
+	sr.db.Delete(segment)
+	return nil
 }
--- app/repositories/segments_repository_test.go
+++ app/repositories/segments_repository_test.go
@@ -14,14 +14,14 @@ func TestSegmentsRepository(t *testing.T) {
 	if err != nil {
 		os.Exit(1)
 	}
-	ur := NewSegmentsRepository(cursor)
+	sr := NewSegmentsRepository(cursor)
 
 	name := "AVITO_VOICE_MESSAGES"
 	segment_add := models.Segments{Name: name}
 	var segmentID uint
 
 	t.Run("Add segment to db", func (t *testing.T) {
-		segment_add, err := ur.Create(segment_add)
+		segment_add, err := sr.Create(segment_add)
 
 		if err != nil {
 			t.Error(err)
@@ -32,7 +32,7 @@ func TestSegmentsRepository(t *testing.T) {
 	})
 
 	t.Run("Find segment by id", func(t *testing.T) {
-		_, err := ur.FindByID(segmentID)
+		_, err := sr.FindByID(segmentID)
 
 		if err != nil {
 			t.Errorf("Segment not found")
@@ -41,10 +41,25 @@ func TestSegmentsRepository(t *testing.T) {
 	})
 
 	t.Run("Find segment by name", func(t *testing.T) {
-		_, err := ur.FindByName(name)
+		_, err := sr.FindByName(name)
 
 		if err != nil {
 			t.Errorf("Segment not found")
+			t.Error(err)
+		}
+	})
+
+	t.Run("Remove segment from database", func(t *testing.T) {
+		err := sr.RemoveSegment(name)
+
+		if err != nil {
+			t.Errorf("Error when removing")
+			t.Error(err)
+		}
+
+		_, err = sr.FindByName(name)
+		if err == nil {
+			t.Errorf("Segment not removed")
 			t.Error(err)
 		}
 	})
