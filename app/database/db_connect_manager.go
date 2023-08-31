ddfa40fbc30d4f80821dfe9fb07bed545efef5d6
Tulenien <timofey.evsigneev@gmail.com>
2023-09-01 01:31:13 +0200

    fix: update

--- app/database/db_connect_manager.go
+++ app/database/db_connect_manager.go
@@ -19,10 +19,10 @@ var DB DbInstance
 func ConnectDB() {
 	dsn := fmt.Sprintf(
 		"host=db user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Moscow",
-		os.Getenv("INPUT_DB_USER"),
-        os.Getenv("INPUT_DB_PASSWORD"),
-        os.Getenv("INPUT_DB_NAME"),
-		os.Getenv("INPUT_DB_PORT"),
+		os.Getenv("DB_USER"),
+        os.Getenv("DB_PASSWORD"),
+        os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
 	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
         Logger: logger.Default.LogMode(logger.Info),
