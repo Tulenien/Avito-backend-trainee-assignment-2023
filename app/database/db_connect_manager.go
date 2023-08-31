d5fee304be0a679d563287a5522fd829070b4bec
Tulenien <timofey.evsigneev@gmail.com>
2023-09-01 00:53:15 +0200

    fix: update

--- app/database/db_connect_manager.go
+++ app/database/db_connect_manager.go
@@ -18,7 +18,11 @@ var DB DbInstance
 
 func ConnectDB() {
 	dsn := fmt.Sprintf(
-		"host=db user=admin password=12345 dbname=avitodb port=5432 sslmode=disable TimeZone=Europe/Moscow"
+		"host=db user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Moscow",
+		os.Getenv("INPUT_DB_USER"),
+        os.Getenv("INPUT_DB_PASSWORD"),
+        os.Getenv("INPUT_DB_NAME"),
+		os.Getenv("INPUT_DB_PORT"),
 	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
         Logger: logger.Default.LogMode(logger.Info),
